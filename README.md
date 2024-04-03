c849173ad092acc2ed89e9cec1b5b88efa1154d5
Daniel <gajardomunoz@gmail.com>
2024-04-03 19:27:49 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Snippets
+
+Personal Code Snippets
---
+++ r-studio.r
@@ -0,0 +1,8 @@
+### R/R Studio Snippets
+
+devtools::install_github("gadenbuie/rsthemes")
+rsthemes::install_rsthemes()
+# rsthemes::install_rsthemes(include_base16 = TRUE)
+
+# Use a theme
+rstudioapi::applyTheme("Material Ocean {rsthemes}")
